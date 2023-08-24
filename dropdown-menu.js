24533a8932993c9077e19e3deb3fbb206fec9c1d
amalgamallama <svetlana.svinina@gmail.com>
2023-08-24 18:42:09 +0300

    Вносит исправления в скрипты

--- dropdown-menu.js
+++
@@ -1,9 +0,0 @@
-// Находим выпадающее меню
-let dropdown = document.querySelector('.nav-dropdown');
-// Находим кнопку открытия меню
-let openDropdown = document.querySelector('.nav-dropdown-button');
-
-// Открыть-закрыть меню
-openDropdown.onclick = function () {
-  dropdown.classList.toggle('nav-dropdown-open')
-};
